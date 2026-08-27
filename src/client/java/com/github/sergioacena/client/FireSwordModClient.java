d5142f2428311c4d8c7417f47fbf48d48bca2ced
Sergio <sergio.acena@gmail.com>
2026-08-28 00:36:48 +0200

    Initial commit: plantilla para Fabric 1.21.11

---
+++ src/client/java/com/github/sergioacena/client/FireSwordModClient.java
@@ -0,0 +1,10 @@
+package com.github.sergioacena.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FireSwordModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
